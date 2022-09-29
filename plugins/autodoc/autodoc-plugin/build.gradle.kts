b6fae71546dc34a97eeefc565ae5909c66aa619e
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2022-09-29 08:14:12 +0200

    change group id, remove publication

--- plugins/autodoc/autodoc-plugin/build.gradle.kts
+++ plugins/autodoc/autodoc-plugin/build.gradle.kts
@@ -14,73 +14,15 @@ gradlePlugin {
             displayName = "autodoc"
             description =
                 "Plugin to generate a documentation manifest for the EDC Metamodel, i.e. extensions, SPIs, etc."
-            id = "autodoc"
-            group = groupId
+            id = "${groupId}.autodoc"
             implementationClass = "org.eclipse.dataspaceconnector.plugins.autodoc.AutodocPlugin"
         }
     }
 }
 
-
-
 pluginBundle {
     website = "https://projects.eclipse.org/proposals/eclipse-dataspace-connector"
-    vcsUrl = "http://github.com/eclipse-dataspaceconnector/GradlePlugins"
-    group = groupId
+    vcsUrl = "https://github.com/eclipse-dataspaceconnector/GradlePlugins.git"
     version = version
     tags = listOf("build", "documentation", "generated", "autodoc")
-}
-
-publishing {
-    afterEvaluate {
-        // values needed for publishing
-        val pluginsWebsiteUrl: String by project
-        val pluginsDeveloperId: String by project
-        val pluginsDeveloperName: String by project
-        val pluginsDeveloperEmail: String by project
-        val pluginsScmConnection: String by project
-        val pluginsScmUrl: String by project
-        publishing {
-            publications.forEach { i ->
-                val mp = (i as MavenPublication)
-                mp.pom {
-                    name.set(project.name)
-                    description.set("edc :: ${project.name}")
-                    url.set(pluginsWebsiteUrl)
-
-                    licenses {
-                        license {
-                            name.set("The Apache License, Version 2.0")
-                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                        }
-                        developers {
-                            developer {
-                                id.set(pluginsDeveloperId)
-                                name.set(pluginsDeveloperName)
-                                email.set(pluginsDeveloperEmail)
-                            }
-                        }
-                        scm {
-                            connection.set(pluginsScmConnection)
-                            url.set(pluginsScmUrl)
-                        }
-                    }
-                }
-//                println("\nset POM for: ${mp.groupId}:${mp.artifactId}:${mp.version}")
-            }
-            signing {
-                useGpgCmd()
-            }
-        }
-    }
-    repositories {
-        maven {
-            name = "OSSRH"
-            setUrl("https://oss.sonatype.org/content/repositories/snapshots/")
-            credentials {
-                username = System.getenv("OSSRH_USER") ?: return@credentials
-                password = System.getenv("OSSRH_PASSWORD") ?: return@credentials
-            }
-        }
-    }
 }
