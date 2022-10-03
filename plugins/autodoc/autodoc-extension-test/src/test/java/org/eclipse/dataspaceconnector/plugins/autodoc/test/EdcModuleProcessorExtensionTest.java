d35ebc8befd7f7c34bd932cb6b41de07234fab41
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2022-10-03 14:17:38 +0200

    feat(processor): enable multiple extensions per module

--- plugins/autodoc/autodoc-extension-test/src/test/java/org/eclipse/dataspaceconnector/plugins/autodoc/test/EdcModuleProcessorExtensionTest.java
+++ plugins/autodoc/autodoc-extension-test/src/test/java/org/eclipse/dataspaceconnector/plugins/autodoc/test/EdcModuleProcessorExtensionTest.java
@@ -18,10 +18,13 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.eclipse.dataspaceconnector.runtime.metamodel.domain.EdcModule;
 import org.eclipse.dataspaceconnector.runtime.metamodel.domain.ServiceReference;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.file.Paths;
 import java.util.List;
 
@@ -34,17 +37,22 @@ class EdcModuleProcessorExtensionTest {
 
     private static final TypeReference<List<EdcModule>> TYPE_REFERENCE = new TypeReference<>() {
     };
+    private URL manifestFileUrl;
+
+    @BeforeEach
+    void setup() throws MalformedURLException {
+        var userDir = System.getProperty("user.dir"); //will point to the module's directory
+        var file = userDir + File.separator + "build" + File.separator + "edc.json";
+        manifestFileUrl = Paths.get(file).toUri().toURL();
+        assertThat(manifestFileUrl).isNotNull();
+    }
 
     @Test
-    void verify() throws IOException {
-        var userdir = System.getProperty("user.dir"); //will point to the module's directory
-        var file = userdir + File.separator + "build" + File.separator + "edc.json";
-        var url = Paths.get(file).toUri().toURL();
-        assertThat(url).isNotNull();
+    void verifyAnnotatedExtension() throws IOException {
 
-        try (var stream = url.openStream()) {
+        try (var stream = manifestFileUrl.openStream()) {
             var manifests = new ObjectMapper().readValue(stream, TYPE_REFERENCE);
-            assertThat(manifests.size()).isEqualTo(1);
+            assertThat(manifests.size()).isEqualTo(2);
 
             var manifest = manifests.get(0);
             assertThat(manifest.getName()).isEqualTo(SampleExtension.NAME);
@@ -67,4 +75,35 @@ class EdcModuleProcessorExtensionTest {
             assertThat(configuration.getDescription()).isNotEmpty();
         }
     }
+
+    @Test
+    void verifyUnannotatedExtension() throws IOException {
+
+
+        try (var stream = manifestFileUrl.openStream()) {
+            var manifests = new ObjectMapper().readValue(stream, TYPE_REFERENCE);
+            assertThat(manifests.size()).isEqualTo(1);
+
+            var manifest = manifests.get(0);
+            assertThat(manifest.getName()).isNull();
+            assertThat(manifest.getCategories()).isEmpty();
+            assertThat(manifest.getOverview()).isNotEmpty();
+
+            var provides = manifest.getProvides();
+            assertThat(provides.size()).isEqualTo(1);
+            assertThat(provides.get(0).getService()).isEqualTo(ProvidedService1.class.getName());
+
+            var references = manifest.getReferences();
+            assertThat(references.size()).isEqualTo(2);
+            assertThat(references).contains(new ServiceReference(OptionalService.class.getName(), false));
+            assertThat(references).contains(new ServiceReference(RequiredService.class.getName(), true));
+
+            var configuration = manifest.getConfiguration().get(0);
+            assertThat(configuration).isNotNull();
+            assertThat(configuration.getKey()).isEqualTo(SampleExtension.CONFIG1);
+            assertThat(configuration.isRequired()).isTrue();
+            assertThat(configuration.getDescription()).isNotEmpty();
+        }
+    }
+
 }
