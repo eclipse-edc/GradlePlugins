c7a0f18441f7909dc1e3b8b12e271f8a70b1420e
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2022-10-07 11:48:19 +0200

    make output directory overrideable

--- plugins/autodoc/autodoc-processor/src/test/java/org/eclipse/dataspaceconnector/plugins/autodoc/core/processor/EdcModuleProcessorTest.java
+++ plugins/autodoc/autodoc-processor/src/test/java/org/eclipse/dataspaceconnector/plugins/autodoc/core/processor/EdcModuleProcessorTest.java
@@ -17,7 +17,12 @@ package org.eclipse.dataspaceconnector.plugins.autodoc.core.processor;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.io.TempDir;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
 
 import java.io.File;
 import java.io.IOException;
@@ -25,7 +30,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import javax.tools.DiagnosticCollector;
 import javax.tools.JavaCompiler;
 import javax.tools.JavaFileObject;
@@ -61,6 +68,45 @@ abstract class EdcModuleProcessorTest {
         task.setProcessors(Collections.singletonList(new EdcModuleProcessor()));
     }
 
+    @ParameterizedTest
+    @ArgumentsSource(ValidCompilerArgsProvider.class)
+    void verifyOptionalCompilerArgs(String edcId, String edcVersion, String edcOutputdir) throws IOException {
+        var compiler = ToolProvider.getSystemJavaCompiler();
+        diagnostics = new DiagnosticCollector<>();
+        var fileManager = compiler.getStandardFileManager(diagnostics, null, null);
+        fileManager.setLocation(StandardLocation.CLASS_OUTPUT, List.of(tempDir.toFile()));
+        fileManager.setLocation(StandardLocation.SOURCE_OUTPUT, List.of(tempDir.toFile()));
+
+
+        var compilationUnits = fileManager.getJavaFileObjects(getCompilationUnits().toArray(File[]::new));
+
+        var options = Stream.of(edcId, edcVersion, edcOutputdir).filter(Objects::nonNull).collect(Collectors.toList());
+        task = compiler.getTask(null, fileManager, diagnostics, options, null, compilationUnits);
+
+        task.setProcessors(Collections.singletonList(new EdcModuleProcessor()));
+        var errorMsg = diagnostics.getDiagnostics().stream().map(Object::toString).collect(Collectors.joining(", "));
+        assertThat(task.call()).withFailMessage(errorMsg).isTrue();
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(InvalidCompilerArgsProvider.class)
+    void verifyRequiredCompilerArgs(String edcId, String edcVersion, String edcOutputdir) throws IOException {
+        var compiler = ToolProvider.getSystemJavaCompiler();
+        diagnostics = new DiagnosticCollector<>();
+        var fileManager = compiler.getStandardFileManager(diagnostics, null, null);
+        fileManager.setLocation(StandardLocation.CLASS_OUTPUT, List.of(tempDir.toFile()));
+        fileManager.setLocation(StandardLocation.SOURCE_OUTPUT, List.of(tempDir.toFile()));
+
+
+        var compilationUnits = fileManager.getJavaFileObjects(getCompilationUnits().toArray(File[]::new));
+
+        var options = Stream.of(edcId, edcVersion, edcOutputdir).filter(Objects::nonNull).collect(Collectors.toList());
+        task = compiler.getTask(null, fileManager, diagnostics, options, null, compilationUnits);
+
+        task.setProcessors(Collections.singletonList(new EdcModuleProcessor()));
+        assertThat(task.call()).isFalse();
+    }
+
     @AfterEach
     void tearDown() {
     }
@@ -83,4 +129,26 @@ abstract class EdcModuleProcessorTest {
 
     protected abstract List<File> getCompilationUnits();
 
+    private static class ValidCompilerArgsProvider implements ArgumentsProvider {
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+            return Stream.of(
+                    Arguments.of("-Aedc.version=1.2.3", "-Aedc.id=someid", "-Aedc.outputDir=some/dir"),
+                    Arguments.of("-Aedc.version=1.2.3", "-Aedc.id=someid", null),
+                    Arguments.of("-Aedc.version=1.2.3", "-Aedc.id=someid", "-Aedc.outputDir=")
+            );
+        }
+    }
+
+    private static class InvalidCompilerArgsProvider implements ArgumentsProvider {
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+            return Stream.of(
+                    Arguments.of("-Aedc.version=1.2.3", "-Aedc.id=", "-Aedc.outputDir=some/dir"),
+                    Arguments.of("-Aedc.version=1.2.3", null, "-Aedc.outputDir=some/dir"),
+                    Arguments.of("-Aedc.version=", "-Aedc.id=someid", "-Aedc.outputDir=some/dir"),
+                    Arguments.of(null, "-Aedc.id=someid", "-Aedc.outputDir=some/dir")
+            );
+        }
+    }
 }
