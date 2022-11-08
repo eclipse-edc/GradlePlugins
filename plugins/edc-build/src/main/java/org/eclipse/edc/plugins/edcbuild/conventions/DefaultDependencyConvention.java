d4257f08e5f8be6a1e75526c7b596e7be7696531
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2022-11-08 11:43:22 +0100

    chore: update groupId to org.eclipse.edc

--- plugins/edc-build/src/main/java/org/eclipse/edc/plugins/edcbuild/conventions/DefaultDependencyConvention.java
+++ plugins/edc-build/src/main/java/org/eclipse/edc/plugins/edcbuild/conventions/DefaultDependencyConvention.java
@@ -27,7 +27,7 @@ import static org.eclipse.edc.plugins.edcbuild.conventions.ConventionFunctions.r
  */
 class DefaultDependencyConvention implements EdcConvention {
 
-    private static final String EDC_GROUP_ID = "org.eclipse.dataspaceconnector";
+    private static final String EDC_GROUP_ID = "org.eclipse.edc";
 
     @Override
     public void apply(Project target) {
