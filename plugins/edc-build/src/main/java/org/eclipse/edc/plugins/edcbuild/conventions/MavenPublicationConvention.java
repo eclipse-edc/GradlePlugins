36dda016b3df63321d337043b52d7bcaab0c310e
Ronja Quensel <ronja.quensel@isst.fraunhofer.de>
2022-12-23 09:41:36 +0100

    feat: add Maven publication convention

---
+++ plugins/edc-build/src/main/java/org/eclipse/edc/plugins/edcbuild/conventions/MavenPublicationConvention.java
@@ -0,0 +1,40 @@
+/*
+ *  Copyright (c) 2022 Fraunhofer Institute for Software and Systems Engineering
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Fraunhofer Institute for Software and Systems Engineering - initial API and implementation
+ *
+ */
+
+package org.eclipse.edc.plugins.edcbuild.conventions;
+
+import org.gradle.api.Project;
+import org.gradle.api.publish.PublishingExtension;
+import org.gradle.api.publish.maven.MavenPublication;
+
+import static org.eclipse.edc.plugins.edcbuild.conventions.ConventionFunctions.requireExtension;
+
+/**
+ * Adds a Maven publication to a project if it does not use the application plugin (library module).
+ */
+public class MavenPublicationConvention implements EdcConvention {
+    
+    @Override
+    public void apply(Project target) {
+        var applicationPlugin = target.getPlugins().findPlugin("application");
+        if (applicationPlugin == null) {
+            requireExtension(target, PublishingExtension.class);
+
+            var pe = target.getExtensions().getByType(PublishingExtension.class);
+            pe.publications(publications -> publications.create(target.getName(), MavenPublication.class,
+                    mavenPublication -> mavenPublication.from(target.getComponents().getByName("java"))));
+        }
+    }
+    
+}
